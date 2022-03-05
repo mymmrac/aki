e25204dc7527525eeb11403afa868d85db821ced
Artem Yadelskyi <mymmrac@gmail.com>
2022-03-05 17:40:50 +0200

    :fire: Added map filters

--- map.go
+++ map.go
@@ -3,7 +3,7 @@ package aki
 // M represents generic map with useful methods
 type M[K comparable, V any] map[K]V
 
-// Values returns values of map with no defined order
+// Values returns values of this map with no defined order
 func (m M[K, V]) Values() []V {
 	values := make([]V, 0, len(m))
 	for _, v := range m {
@@ -12,11 +12,78 @@ func (m M[K, V]) Values() []V {
 	return values
 }
 
-// Keys returns keys of map with no defined order
+// MValues returns values of specified map with no defined order
+func MValues[K comparable, V any](m map[K]V) []V {
+	return M[K, V](m).Values()
+}
+
+// Keys returns keys of this map with no defined order
 func (m M[K, V]) Keys() []K {
 	keys := make([]K, 0, len(m))
 	for k := range m {
 		keys = append(keys, k)
 	}
 	return keys
+}
+
+// MKeys returns keys of specified map with no defined order
+func MKeys[K comparable, V any](m map[K]V) []K {
+	return M[K, V](m).Keys()
+}
+
+// MPredicate defines map predicate
+type MPredicate[K comparable, V any] func(key K, values V) bool
+
+// MPredicateByKey defines map predicate by key
+type MPredicateByKey[K comparable] func(key K) bool
+
+// MPredicateByValue defines map predicate by value
+type MPredicateByValue[V any] func(values V) bool
+
+// Filter returns new map from this, filtered by key and value using provided predicate
+func (m M[K, V]) Filter(predicate MPredicate[K, V]) M[K, V] {
+	filtered := make(M[K, V])
+	for key, value := range m {
+		if predicate(key, value) {
+			filtered[key] = value
+		}
+	}
+	return filtered
+}
+
+// MFilter returns new map from specified, filtered by key and value using provided predicate
+func MFilter[K comparable, V any](m map[K]V, predicate MPredicate[K, V]) map[K]V {
+	return M[K, V](m).Filter(predicate)
+}
+
+// FilterByKey returns new map from this, filtered by key using provided predicate
+func (m M[K, V]) FilterByKey(predicate MPredicateByKey[K]) M[K, V] {
+	filtered := make(M[K, V])
+	for key, value := range m {
+		if predicate(key) {
+			filtered[key] = value
+		}
+	}
+	return filtered
+}
+
+// MFilterByKey returns new map from specified, filtered by key using provided predicate
+func MFilterByKey[K comparable, V any](m map[K]V, predicate MPredicateByKey[K]) map[K]V {
+	return M[K, V](m).FilterByKey(predicate)
+}
+
+// FilterByValue returns new map from this, filtered by value using provided predicate
+func (m M[K, V]) FilterByValue(predicate MPredicateByValue[V]) M[K, V] {
+	filtered := make(M[K, V])
+	for key, value := range m {
+		if predicate(value) {
+			filtered[key] = value
+		}
+	}
+	return filtered
+}
+
+// MFilterByValue returns new map from specified, filtered by value using provided predicate
+func MFilterByValue[K comparable, V any](m map[K]V, predicate MPredicateByValue[V]) map[K]V {
+	return M[K, V](m).FilterByValue(predicate)
 }
--- map_test.go
+++ map_test.go
@@ -8,33 +8,63 @@ import (
 
 var mapTestCases = []struct {
 	name   string
-	m      M[int, int]
+	m      M[int, float64]
 	keys   []int
-	values []int
+	values []float64
+	fp     MPredicate[int, float64]
+	fm     M[int, float64]
+	fkp    MPredicateByKey[int]
+	fkm    M[int, float64]
+	fvp    MPredicateByValue[float64]
+	fvm    M[int, float64]
 }{
 	{
 		name:   "nil",
 		m:      nil,
 		keys:   []int{},
-		values: []int{},
+		values: []float64{},
+		fp:     func(_ int, _ float64) bool { return true },
+		fm:     map[int]float64{},
+		fkp:    func(_ int) bool { return true },
+		fkm:    map[int]float64{},
+		fvp:    func(_ float64) bool { return true },
+		fvm:    map[int]float64{},
 	},
 	{
 		name:   "empty",
-		m:      make(map[int]int),
+		m:      map[int]float64{},
 		keys:   []int{},
-		values: []int{},
+		values: []float64{},
+		fp:     func(_ int, _ float64) bool { return true },
+		fm:     map[int]float64{},
+		fkp:    func(_ int) bool { return true },
+		fkm:    map[int]float64{},
+		fvp:    func(_ float64) bool { return true },
+		fvm:    map[int]float64{},
 	},
 	{
 		name:   "one_value",
-		m:      M[int, int]{1: 2},
+		m:      M[int, float64]{1: 2},
 		keys:   []int{1},
-		values: []int{2},
+		values: []float64{2},
+		fp:     func(_ int, _ float64) bool { return false },
+		fm:     map[int]float64{},
+		fkp:    func(_ int) bool { return false },
+		fkm:    map[int]float64{},
+		fvp:    func(_ float64) bool { return false },
+		fvm:    map[int]float64{},
 	},
 	{
 		name:   "multiple_value",
-		m:      M[int, int]{1: 2, 3: 4, 5: 6, 7: 8},
+		m:      M[int, float64]{1: 2, 3: 4, 5: 6, 7: 8},
 		keys:   []int{1, 3, 5, 7},
-		values: []int{2, 4, 6, 8},
+		values: []float64{2, 4, 6, 8},
+		fp:     func(key int, value float64) bool { return key == 1 || value == 4 },
+		fm:     map[int]float64{1: 2, 3: 4},
+		fkp:    func(key int) bool { return key == 1 },
+		fkm:    map[int]float64{1: 2},
+		fvp:    func(value float64) bool { return value == 4 },
+		fvm:    map[int]float64{3: 4},
 	},
 }
 
@@ -50,6 +80,70 @@ func TestM_Keys(t *testing.T) {
 	for _, tt := range mapTestCases {
 		t.Run(tt.name, func(t *testing.T) {
 			assert.ElementsMatch(t, tt.keys, tt.m.Keys())
+		})
+	}
+}
+
+func TestMValues(t *testing.T) {
+	for _, tt := range mapTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.ElementsMatch(t, tt.values, MValues(tt.m))
+		})
+	}
+}
+
+func TestMKeys(t *testing.T) {
+	for _, tt := range mapTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.ElementsMatch(t, tt.keys, MKeys(tt.m))
+		})
+	}
+}
+
+func TestM_Filter(t *testing.T) {
+	for _, tt := range mapTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.fm, tt.m.Filter(tt.fp))
+		})
+	}
+}
+
+func TestMFilter(t *testing.T) {
+	for _, tt := range mapTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, map[int]float64(tt.fm), MFilter(tt.m, tt.fp))
+		})
+	}
+}
+
+func TestM_FilterByKey(t *testing.T) {
+	for _, tt := range mapTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.fkm, tt.m.FilterByKey(tt.fkp))
+		})
+	}
+}
+
+func TestMFilterByKey(t *testing.T) {
+	for _, tt := range mapTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, map[int]float64(tt.fkm), MFilterByKey(tt.m, tt.fkp))
+		})
+	}
+}
+
+func TestM_FilterByValue(t *testing.T) {
+	for _, tt := range mapTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.fvm, tt.m.FilterByValue(tt.fvp))
+		})
+	}
+}
+
+func TestMFilterByValue(t *testing.T) {
+	for _, tt := range mapTestCases {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, map[int]float64(tt.fvm), MFilterByValue(tt.m, tt.fvp))
 		})
 	}
 }
